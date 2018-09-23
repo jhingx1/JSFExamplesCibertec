e6ff22a80fc951c8a804978c5f68d03105f556b3
jhingx1 <jhingx1@gmail.com>
2018-09-22 21:11:41 -0500

    Ejmplo de Formulario JSF
    Formulario en JSF, ver la navegabilidad. Paso de parametros y muestra datos.

---
+++ nav02/src/java/web/bean/ManagedBeanData.java
@@ -0,0 +1,45 @@
+package web.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name = "data")
+@RequestScoped
+public class ManagedBeanData {
+
+    private String apellidos;
+    private String nombres;
+    private Double sueldo;
+
+    public ManagedBeanData() {
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public Double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(Double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public String navega() {
+        return "result";
+    }
+
+}
