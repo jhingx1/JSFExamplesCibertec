69433ebc9458147dce652bb00f8fdc8c4efca3d0
jhingx1 <jhingx1@gmail.com>
2018-10-02 00:18:16 -0500

    Ejm enlanzado campos-binding
    El valor 1 esta enlazado (binding) al valor 2.

--- binding/src/java/web/validator/ValidaPar.java
+++ binding/src/java/web/validator/ValidaPar.java
@@ -13,22 +13,29 @@ public class ValidaPar implements Validator{
     @Override
     public void validate(FacesContext fc, UIComponent uic, Object o)
             throws ValidatorException {
+        //Capturando el valor que se pasa desde la vista.
         String valor = o.toString().trim();
         if (valor.length() == 0) {
             throw new ValidatorException(
                     new FacesMessage("ingrese entero par"));
         } else {
             try {
-                Integer x = Integer.valueOf(valor);
+                //Uso de Integer.valueOf(valor), para pasar de String a su forma de entero
+                //ademas esto se puede usar como un validador de que se esta enviado un
+                //entero.
+                Integer x = Integer.valueOf(valor);//pasando a entero
                 if ((x % 2) == 1) {
                     throw new ValidatorException(
                             new FacesMessage("debe ser par"));
                 }
             } catch (NumberFormatException e) {
+                //Aquie se ve como se captura si se ejecuta un error
+                //significa que no se esta pasando un numero.
                 throw new ValidatorException(
                         new FacesMessage("valor entero"));
             }
         }
+        
     }
 
 }
