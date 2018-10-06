1f82c5713cb22463534948aa63bfbf18b4c24411
jhingx1 <jhingx1@gmail.com>
2018-10-06 10:01:47 -0500

    Plantilla de JSF con primeface
    Ejm con fecha y texto.

---
+++ Plantillas-Jsf-Prinface/src/java/dto/Cursos.java
@@ -0,0 +1,59 @@
+package dto;
+
+//import java.sql.Date;
+
+import java.util.Date;
+
+//import java.sql.Timestamp;
+
+public class Cursos {
+    
+    private Integer idcurso;
+    private String titulo;
+    private Integer horas;
+    private String horario;
+    private Date fechainicio; 
+
+    public Cursos() {
+    }
+    
+    public Integer getIdcurso() {
+        return idcurso;
+    }
+
+    public void setIdcurso(Integer idcurso) {
+        this.idcurso = idcurso;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getHoras() {
+        return horas;
+    }
+
+    public void setHoras(Integer horas) {
+        this.horas = horas;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    public Date getFechainicio() {
+        return fechainicio;
+    }
+
+    public void setFechainicio(Date fechainicio) {
+        this.fechainicio = fechainicio;
+    }
+}
